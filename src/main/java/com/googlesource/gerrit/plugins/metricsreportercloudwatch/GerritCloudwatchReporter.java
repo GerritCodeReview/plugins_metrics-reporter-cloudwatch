37e5c7d4252dadb19c42dc48882d78466800873b
Antonio Barone <syntonyze@gmail.com>
2020-07-14 14:32:44 +0300

    Add ability to exclude metrics
    Handle exclusion rules so that the matching metrics are not reported to
    CloudWatch.
    This is useful to contain the metrics to only the wanted ones,
    minimizing network traffic and, crucially, AWS bills.
    
    This change is an adaptation of the approach used by [1] and by [2].
    
    [1] https://gerrit-review.googlesource.com/c/plugins/metrics-reporter-prometheus/+/253638
    [2] https://gerrit-review.googlesource.com/c/plugins/metrics-reporter-jmx/+/253634
    
    Feature: Issue 13148
    Change-Id: I82635cd152582e7f10eff8ebbe0eeaf29d868e7e

--- src/main/java/com/googlesource/gerrit/plugins/metricsreportercloudwatch/GerritCloudwatchReporter.java
+++ src/main/java/com/googlesource/gerrit/plugins/metricsreportercloudwatch/GerritCloudwatchReporter.java
@@ -13,7 +13,6 @@
 // limitations under the License.
 package com.googlesource.gerrit.plugins.metricsreportercloudwatch;
 
-import com.codahale.metrics.MetricFilter;
 import com.codahale.metrics.MetricRegistry;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.flogger.FluentLogger;
@@ -45,7 +44,7 @@ public class GerritCloudwatchReporter implements LifecycleListener {
                 registry, cloudWatchAsyncClientBuilder.build(), config.getNamespace())
             .convertRatesTo(TimeUnit.SECONDS)
             .convertDurationsTo(TimeUnit.MILLISECONDS)
-            .filter(MetricFilter.ALL)
+            .filter(config.getExclusionFilter())
             .withZeroValuesSubmission()
             .withReportRawCountValue()
             .withHighResolution();
