bba13fbf199435b38f1771f771c12f5ebacdd3c6
Antonio Barone <syntonyze@gmail.com>
2020-08-06 10:46:56 +0200

    Correct unnecessary builder reassignment
    codahale-aggregated-metrics-cloudwatch-reporter's with* methods are
    mutating the builder object itself, so there's no need to reassign the
    mutated object to itself from the caller.
    
    This allows to make the CloudWatchAsyncClientBuilder final, which in
    turns helps the compiler, improves readability and allows to use the
    value in lambdas.
    
    Change-Id: I0bdf608f1c9649f4a139eb3fdd6b6fc8062041d8

--- src/main/java/com/googlesource/gerrit/plugins/metricsreportercloudwatch/GerritCloudwatchReporter.java
+++ src/main/java/com/googlesource/gerrit/plugins/metricsreportercloudwatch/GerritCloudwatchReporter.java
@@ -33,7 +33,8 @@ public class GerritCloudwatchReporter implements LifecycleListener {
       throws IllegalStateException {
     this.config = config;
 
-    CloudWatchAsyncClientBuilder cloudWatchAsyncClientBuilder = CloudWatchAsyncClient.builder();
+    final CloudWatchAsyncClientBuilder cloudWatchAsyncClientBuilder =
+        CloudWatchAsyncClient.builder();
 
     CloudWatchReporter.Builder cloudWatchReporterBuilder =
         CloudWatchReporter.forRegistry(
@@ -46,11 +47,11 @@ public class GerritCloudwatchReporter implements LifecycleListener {
             .withHighResolution();
 
     if (config.getDryRun()) {
-      cloudWatchReporterBuilder = cloudWatchReporterBuilder.withDryRun();
+      cloudWatchReporterBuilder.withDryRun();
     }
 
     if (config.getJvmMetrics()) {
-      cloudWatchReporterBuilder = cloudWatchReporterBuilder.withJvmMetrics();
+      cloudWatchReporterBuilder.withJvmMetrics();
     }
 
     cloudWatchReporter = cloudWatchReporterBuilder.build();
